caaaf09463847601c01f577a1242ae153fce3f60
Iwan Firmawan <firmawaneiwan@gmail.com>
2022-05-22 21:07:39 +0700

    feature: create jokes routes and adding css

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import type { LinksFunction } from "@remix-run/node";
 import type { MetaFunction } from "@remix-run/node";
 import {
   Links,
@@ -8,9 +9,32 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import globalStylesUrl from "./styles/global.css";
+import globalMediumStylesUrl from "./styles/global-medium.css";
+import globalLargeStylesUrl from "./styles/global-large.css";
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: globalStylesUrl,
+    },
+    {
+      rel: "stylesheet",
+      href: globalMediumStylesUrl,
+      media: "print, (min-width: 640px)",
+    },
+    {
+      rel: "stylesheet",
+      href: globalLargeStylesUrl,
+      media: "screen and (min-width: 1024px)",
+    },
+  ];
+};
+
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Remix: Jokes app",
   viewport: "width=device-width,initial-scale=1",
 });
 
@@ -23,8 +47,6 @@ export default function App() {
       </head>
       <body>
         <Outlet />
-        <ScrollRestoration />
-        <Scripts />
         <LiveReload />
       </body>
     </html>
